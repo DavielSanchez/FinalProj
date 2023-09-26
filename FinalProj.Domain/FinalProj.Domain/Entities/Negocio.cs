bbcf57133205ae78fe4d2a581c3c2e48448ea374
NDHBBCYED <clementebatista70@gmail.com>
2023-09-25 20:01:09 -0400

    Update README.md

---
+++ FinalProj.Domain/FinalProj.Domain/Entities/Negocio.cs
@@ -0,0 +1,22 @@
+﻿using FinalProj.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProj.Domain.Entities
+{
+    public  class Negocio : BaseEntity
+    {
+        public int Id { get; set; }
+        public string? UrlLogo { get; set; }
+        public string? NombreLogo { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Direccion { get; set; }
+        public  new string? FechaRegistro { get; set; }
+        public string? Telefono { get; set; }
+        public decimal? PorcentajeImpuesto { get; set; }
+        public string? SimboloMoneda { get; set; }
+    }
+}
